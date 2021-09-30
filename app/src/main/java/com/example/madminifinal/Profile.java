f08c65c9be8a4c1a562a2fe88eba37eb0e446462
prabhaviweerasinghe <88457635+prabhaviweerasinghe@users.noreply.github.com>
2021-10-01 04:50:20 +0530

    IT20242190 - P.P.Weerasinghe

---
+++ app/src/main/java/com/example/madminifinal/Profile.java
@@ -0,0 +1,30 @@
+package com.example.madminifinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Profile extends AppCompatActivity {
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle saveInstanceState) {
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        btn = findViewById((R.id.profile));
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), MyInfo.class);
+                startActivity(i);
+            }
+        });
+    }
+}
+
+
+
